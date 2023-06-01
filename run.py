3e0c37c7bd86d8e0fb26180428771a4587f77ea1
lukecdev <courtney.luke@outlook.com>
2023-06-01 15:23:24 +0000

    Adding basic game structure to run.py

--- run.py
+++ run.py
@@ -2,3 +2,14 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def game_start():
+    """
+    Start function with game instructions 
+    """
+
+def main():
+    """
+    Function to run the game from the beginning
+    """
+
+main()
